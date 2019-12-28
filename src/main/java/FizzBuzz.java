a75031a065605d06d061c91fa5a801eacab88897
alai1716 <393687596@qq.com>
2019-12-28 14:11:49 +0800

    refactor

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,19 @@
+public class FizzBuzz {
+
+    public static String of(int number) {
+        String result = "";
+        if (number % 3 == 0 && number % 5 == 0) {
+            result = "FizzBuzz";
+        } else if (number % 3 == 0) {
+            result = "Fizz";
+        } else if (number % 5 == 0) {
+            result = "Buzz";
+        }
+
+        if (result.equals("")) {
+
+            return String.valueOf(number);
+        }
+        return result;
+    }
+}
