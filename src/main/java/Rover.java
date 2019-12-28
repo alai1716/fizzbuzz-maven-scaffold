979b05d29b3dbb2cc7d5f5a83dcd3f19e51f16e9
alai1716 <393687596@qq.com>
2019-12-28 17:19:42 +0800

    refactor

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -9,10 +9,10 @@ public class Rover {
 	private String direction;
 	
 	public void land(Area area, int x, int y, String direction) throws Exception {
-		if(!area.containsPointWithX(x)){
+		if (!area.containsPointWithX(x)) {
 			throw new Exception("x=" + x + " is out of area");
 		}
-		if(!area.containsPointWithY(y)){
+		if (!area.containsPointWithY(y)) {
 			throw new Exception("Y=" + y + " is out of area");
 		}
 		this.area = area;
@@ -26,59 +26,59 @@ public class Rover {
 	}
 
 	public void move() throws Exception {
-		if(NORTH.equals(direction)){
+		if (NORTH.equals(direction)) {
 			y++;
-		} else if(SOUTH.equals(direction) && y > 0){
+		} else if (SOUTH.equals(direction) && y > 0) {
 			y--;
-		} else if(EAST.equals(direction)){
+		} else if (EAST.equals(direction)) {
 			x++;
-		} else if(WEST.equals(direction) && x > 0){
+		} else if (WEST.equals(direction) && x > 0) {
 			x--;
 		}
 		
-		if(!area.containsPointWithX(x)){
+		if (!area.containsPointWithX(x)) {
 			throw new Exception("不能移动到区域外");
 		}
 		
-		if(!area.containsPointWithY(y)){
+		if (!area.containsPointWithY(y)) {
 			throw new Exception("不能移动到区域外");
 		}
 	}
 	
 	public void turnLeft() {
-		if(NORTH.equals(direction)){
+		if (NORTH.equals(direction)) {
     		direction = WEST;
-		} else if(SOUTH.equals(direction)){
+		} else if (SOUTH.equals(direction)) {
 			direction = EAST;
-		} else if(EAST.equals(direction)){
+		} else if (EAST.equals(direction)) {
 			direction = NORTH;
-		} else if(WEST.equals(direction)){
+		} else if (WEST.equals(direction)) {
 			direction = SOUTH;
 		}
 	}
 	
     public void turnRight() {
-    	if(NORTH.equals(direction)){
+    	if (NORTH.equals(direction)) {
     		direction = EAST;
-		} else if(SOUTH.equals(direction)){
+		} else if (SOUTH.equals(direction)) {
 			direction = WEST;
-		} else if(EAST.equals(direction)){
+		} else if (EAST.equals(direction)) {
 			direction = SOUTH;
-		} else if(WEST.equals(direction)){
+		} else if (WEST.equals(direction)) {
 			direction = NORTH;
 		}
 	}
 	
 	public String execute(String mission) throws Exception {
 		String[] info = mission.split(",");
-		if(info.length < 5){
+		if (info.length < 5) {
 			return null;
 		}	
 		Area area = new Area(Integer.parseInt(info[0]), Integer.parseInt(info[1]));
 		Rover rover = new Rover();
 		rover.land(area, Integer.parseInt(info[2]), Integer.parseInt(info[3]), info[4]);
 		
-		for(int i = 4; i < info.length; i++){
+		for (int i = 4; i < info.length; i++) {
 			executeOneCommend(info, rover, i);
 		}
 		
@@ -86,11 +86,11 @@ public class Rover {
 	}
 
 	private void executeOneCommend(String[] info, Rover rover, int i) throws Exception {
-		if("M".equals(info[i])){
+		if ("M".equals(info[i])) {
 			rover.move();
-		} else if("L".equals(info[i])){
+		} else if ("L".equals(info[i])) {
 			rover.turnLeft();
-		} else if("R".equals(info[i])){
+		} else if ("R".equals(info[i])) {
 			rover.turnRight();
 		}
 	}
